5a238aac1a0577fd9ffb64e213289621ec8070ec
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-12 02:10:19 +0900

    fix memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 16:55:14 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/10/07 17:08:35 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/11 17:10:00 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*p_dst;
 	unsigned char	*p_src;
+	size_t			i;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
 	p_dst = (unsigned char *)dst;
 	p_src = (unsigned char *)src;
-	while (n--)
+	i = 0;
+	while (i < n)
 	{
-		*p_dst = *p_src;
-		p_dst++;
-		p_src++;
+		p_dst[i] = p_src[i];
+		i++;
 	}
 	return (dst);
 }
