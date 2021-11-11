085cf8be47038e7465c1ebb429377dc996cb1f72
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-12 03:03:39 +0900

    fix strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 18:12:25 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/10/31 05:58:21 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/11 17:59:19 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
+	char	chr;
 	size_t	i;
-	char	ch;
 
-	ch = (char)c;
+	chr = (char)c;
 	i = ft_strlen(s);
 	while (true)
 	{
-		if (s[i] == ch)
+		if (s[i] == chr)
 			return ((char *)&s[i]);
 		if (i == 0)
 			break ;
