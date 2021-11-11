bac7bf45ad353dd3fd6beb1062a4618edac8649d
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-12 02:46:29 +0900

    fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 14:45:29 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/10/07 17:19:00 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/11 17:38:03 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*p_dest;
 	unsigned char	*p_src;
+	size_t			i;
 
 	if (dest == NULL && src == NULL)
 		return (NULL);
 	p_dest = (unsigned char *)dest;
 	p_src = (unsigned char *)src;
+	i = 0;
 	if (p_dest < p_src)
 	{
-		while (n--)
+		while (i < n)
 		{
-			*p_dest = *p_src;
-			p_dest++;
-			p_src++;
+			p_dest[i] = p_src[i];
+			i++;
 		}
 	}
 	else
 	{
-		while (n--)
-			p_dest[n] = p_src[n];
+		while (i++ < n)
+			p_dest[n - i] = p_src[n - i];
 	}
 	return (dest);
 }
