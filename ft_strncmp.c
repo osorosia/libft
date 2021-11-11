606eac5a5e98eb09698585d74e3c1e3504fc390f
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-12 03:07:36 +0900

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 13:34:39 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/10/31 11:10:51 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/11 18:06:39 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned char	*s1_ptr;
 	unsigned char	*s2_ptr;
+	size_t			i;
 
 	s1_ptr = (unsigned char *)s1;
 	s2_ptr = (unsigned char *)s2;
-	while (n > 0)
+	i = 0;
+	while (i < n)
 	{
-		if (*s1_ptr != *s2_ptr || *s1_ptr == '\0')
-			return ((int)*s1_ptr - (int)*s2_ptr);
-		s1_ptr++;
-		s2_ptr++;
-		n--;
+		if (s1_ptr[i] != s2_ptr[i] || s1_ptr[i] == '\0')
+			return ((int)s1_ptr[i] - (int)s2_ptr[i]);
+		i++;
 	}
 	return (0);
 }
