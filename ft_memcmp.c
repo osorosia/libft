638c4846efab342580a8e512fc307f35568c126d
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-12 10:30:42 +0900

    fix memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 18:38:03 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/11/11 18:15:23 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/12 01:30:28 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	ptr1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
+	i = 0;
 	while (i < n)
 	{
 		if (ptr1[i] != ptr2[i])
