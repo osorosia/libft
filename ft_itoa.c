1749e21d4e86484461bea6f2c70e827206cacd31
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-13 09:49:57 +0900

    static関数名をアンダーバーprefixに変更

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,29 +6,29 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 18:23:13 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/11/01 19:28:12 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/13 00:48:51 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_abs(int n)
+static int	_abs(int n)
 {
 	if (n < 0)
 		return (-1 * n);
 	return (n);
 }
 
-static void	get_str(char *str, size_t str_i, int n)
+static void	_get_str(char *str, size_t str_i, int n)
 {
 	if (n < 0)
 		str[0] = '-';
-	str[str_i] = ft_abs(n % 10) + '0';
+	str[str_i] = _abs(n % 10) + '0';
 	if (n / 10 != 0)
-		get_str(str, str_i - 1, ft_abs(n / 10));
+		_get_str(str, str_i - 1, _abs(n / 10));
 }
 
-static size_t	get_str_len(int n)
+static size_t	_get_str_len(int n)
 {
 	size_t	str_len;
 
@@ -50,11 +50,11 @@ char	*ft_itoa(int n)
 	char	*str;
 	size_t	str_len;
 
-	str_len = get_str_len(n);
+	str_len = _get_str_len(n);
 	str = (char *)malloc(sizeof(char) * (str_len + 1));
 	if (str == NULL)
 		return (NULL);
-	get_str(str, str_len - 1, n);
+	_get_str(str, str_len - 1, n);
 	str[str_len] = '\0';
 	return (str);
 }
