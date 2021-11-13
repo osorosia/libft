9871d13897d7e14f5300e3d313379667f7884a3b
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-13 09:43:19 +0900

    static関数の命名をstaticらしいものに変更

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 23:46:44 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/11/03 07:04:27 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/13 00:42:40 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_strs_len(char const *s, char c)
+static size_t	_get_strs_len(char const *s, char c)
 {
 	size_t	strs_len;
 
@@ -26,7 +26,7 @@ static size_t	get_strs_len(char const *s, char c)
 	return (strs_len);
 }
 
-static size_t	get_str_len(char const *s, char c)
+static size_t	_get_str_len(char const *s, char c)
 {
 	size_t	str_len;
 
@@ -39,12 +39,12 @@ static size_t	get_str_len(char const *s, char c)
 	return (str_len);
 }
 
-static char	*get_str(char const *s, size_t str_len)
+static char	*_get_str(char const *s, size_t str_len)
 {
 	return (ft_substr(s, 0, str_len));
 }
 
-static bool	get_strs_recursive(char **strs, char const *s, char c)
+static bool	_get_strs_recursive(char **strs, char const *s, char c)
 {
 	size_t	str_len;
 
@@ -55,11 +55,11 @@ static bool	get_strs_recursive(char **strs, char const *s, char c)
 		*strs = NULL;
 		return (true);
 	}
-	str_len = get_str_len(s, c);
-	*strs = get_str(s, str_len);
+	str_len = _get_str_len(s, c);
+	*strs = _get_str(s, str_len);
 	if (*strs == NULL)
 		return (false);
-	if (!get_strs_recursive(strs + 1, s + str_len, c))
+	if (!_get_strs_recursive(strs + 1, s + str_len, c))
 	{
 		free(*strs);
 		return (false);
@@ -74,11 +74,11 @@ char	**ft_split(char const *s, char c)
 
 	if (s == NULL)
 		return (NULL);
-	strs_len = get_strs_len(s, c);
+	strs_len = _get_strs_len(s, c);
 	strs = (char **)malloc(sizeof(char *) * (strs_len + 1));
 	if (strs == NULL)
 		return (NULL);
-	if (!get_strs_recursive(strs, s, c))
+	if (!_get_strs_recursive(strs, s, c))
 	{
 		free(strs);
 		return (NULL);
