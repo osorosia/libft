b310e7e8b214a97f28e9f4b923e31a31706ba326
osorosia <64416398+osorosia@users.noreply.github.com>
2021-11-15 21:52:34 +0900

    不要なincludeを削除

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 20:47:03 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/11/11 20:28:22 by rnishimo         ###   ########.fr       */
+/*   Updated: 2021/11/13 10:08:21 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 
 # include <limits.h>
 # include <stdint.h>
-# include <stdio.h>
 # include <stddef.h>
-# include <string.h>
 # include <stdlib.h>
 # include <stdbool.h>
 # include <unistd.h>
