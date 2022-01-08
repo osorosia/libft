1cd09af3340c03a48638606668e19c0b69f7536b
osorosia <64416398+osorosia@users.noreply.github.com>
2022-01-08 07:50:41 +0100

    引数名を変更: new -> new_lst

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,24 +6,24 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 17:17:44 by rnishimo          #+#    #+#             */
-/*   Updated: 2021/11/13 06:25:36 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/01/08 07:49:31 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_lst)
 {
 	t_list	*lst_last;
 
-	if (lst == NULL || new == NULL)
+	if (lst == NULL || new_lst == NULL)
 		return ;
 	if (*lst == NULL)
 	{
-		*lst = new;
+		*lst = new_lst;
 		return ;
 	}
 	lst_last = ft_lstlast(*lst);
 	if (lst_last != NULL)
-		lst_last->next = new;
+		lst_last->next = new_lst;
 }
