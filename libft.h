4ec6507e445433ea85990ecd5b80345fee2dd40c
osorosia <64416398+osorosia@users.noreply.github.com>
2022-05-28 14:30:41 +0900

    fix header

--- libft.h
+++ libft.h
@@ -32,42 +32,33 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-int			ft_abs(int num);
-long		ft_abs_l(long num);
-long long	ft_abs_ll(long long num);
-int			ft_atoi(const char *str);
-void		ft_bzero(void *s, size_t n);
-void		*ft_calloc(size_t count, size_t size);
+// ft_is
 int			ft_isalnum(int c);
 int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 bool		ft_isnumber(char *str);
 int			ft_isprint(int c);
-char		*ft_itoa(int n);
-void		ft_lstadd_back(t_list **lst, t_list *new_lst);
-void		ft_lstadd_front(t_list **lst, t_list *new_lst);
-void		ft_lstclear(t_list **lst, void (*del)(void*));
-void		ft_lstdelone(t_list *lst, void (*del)(void*));
-void		ft_lstiter(t_list *lst, void (*f)(void *));
-t_list		*ft_lstlast(t_list *lst);
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-t_list		*ft_lstnew(void *content);
-int			ft_lstsize(t_list *lst);
+
+// ft_mem
+void		ft_bzero(void *s, size_t n);
+void		*ft_calloc(size_t count, size_t size);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 void		*ft_memset(void *buf, int ch, size_t n);
-size_t		ft_numlen(long long num);
-size_t		ft_numlen_base(long long num, int base);
-size_t		ft_numlen_u(unsigned long long num);
-size_t		ft_numlen_u_base(unsigned long long num, unsigned int base);
+
+// ft_put
 void		ft_putchar_fd(char c, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
 void		ft_putnstr_fd(char *s, size_t n, int fd);
 void		ft_putstr_fd(char *s, int fd);
+
+// ft_str
+int			ft_atoi(const char *str);
+char		*ft_itoa(int n);
 char		**ft_split(char const *s, char c);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strdup(const char *s1);
@@ -87,6 +78,8 @@ char		*ft_strtrim(char const *s1, char const *set);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
+
+// get_next_line
 char		*get_next_line(int fd);
 
 #endif
