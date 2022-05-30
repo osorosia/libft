110491f0a261b8f2e19ee0b5ab7d9a9ef4ff826a
osorosia <64416398+osorosia@users.noreply.github.com>
2022-05-30 16:52:10 +0900

    refactor

--- ft_x/ft_xstrdup.c
+++ ft_x/ft_xstrdup.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 18:02:27 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/05/29 18:02:29 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/05/30 16:51:06 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,12 @@ char	*ft_xstrdup(const char *s1)
 	size_t	i;
 
 	len = ft_strlen(s1);
-	str = (char *)malloc(sizeof(char) * len + 1);
-	if (str == NULL)
-	{
-		perror("malloc");
-		exit(1);
-	}
+	str = ft_xcalloc(len + 1, sizeof(char));
 	i = 0;
 	while (i < len)
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	str[i] = '\0';
 	return (str);
 }
--- ft_x/ft_xstrndup.c
+++ ft_x/ft_xstrndup.c
@@ -6,7 +6,7 @@
 /*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 18:02:30 by rnishimo          #+#    #+#             */
-/*   Updated: 2022/05/29 18:02:31 by rnishimo         ###   ########.fr       */
+/*   Updated: 2022/05/30 16:51:56 by rnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,12 @@ char	*ft_strndup(const char *s1, size_t n)
 	char	*str;
 	size_t	i;
 
-	str = (char *)malloc((n + 1) * sizeof(char));
-	if (str == NULL)
-	{
-		perror("malloc");
-		exit(1);
-	}
+	str = ft_xcalloc(n + 1, sizeof(char));
 	i = 0;
 	while (i < n)
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	str[i] = '\0';
 	return (str);
 }
